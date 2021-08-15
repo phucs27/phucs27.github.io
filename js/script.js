3f2c8d778710c67478fcf05bbb0af897b35809d9
phucs27 <doanvanphuc27@gmail.com>
2021-08-16 00:00:15 +0700

    up tiep 12:00

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(function(){
-    vitri = $('#edu').offset().top;
     $(".roller, .exp").click(function(){
-        $('body, html').animate({scrollTop:vitri})
+        $('body, html').animate({scrollTop:$('#edu').offset().top});
+        return false;
+    })  
+
+    $(".sp").click(function(){
+        $('body, html').animate({scrollTop:$('#portfolio').offset().top});
+        return false;
+    })
+    $(".cont").click(function(){
+        $('body, html').animate({scrollTop:$('#contact').offset().top});
+        return false;
     })
 })
+
