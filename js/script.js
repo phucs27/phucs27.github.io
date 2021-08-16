522c0b9d9b2041b525bed80861b99dcdfec73c0e
phucs27 <doanvanphuc27@gmail.com>
2021-08-17 01:14:34 +0700

    update portfolio

--- js/script.js
+++ js/script.js
@@ -13,4 +13,12 @@ $(function(){
         return false;
     })
 })
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
 
