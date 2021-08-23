6f812d2bbbc064b833aee896f1aa765d9a6f4daa
phucs27 <doanvanphuc27@gmail.com>
2021-08-23 16:30:46 +0700

    ...

--- js/send-mail.js
+++ js/send-mail.js
@@ -1,4 +1,4 @@
-function sendMail(params){
+/*function sendMail(params){
     var tempParams ={
         from_name:document.getElementById("fromEmail").value,
         to_name:document.getElementById("toName").value,
@@ -7,4 +7,4 @@ function sendMail(params){
     emailjs.send('gmail','template_wwn4x38',tempParams).then(function(res){
         console.log("success", res.status);
     })
-}
+}*/
