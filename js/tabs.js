fdf8f7b129b7818c193db7729751943f0cad347b
phucs27 <doanvanphuc27@gmail.com>
2021-08-25 00:17:06 +0700

    ...

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
     $('.tabs div').click(function(){
         var tab = $(this).data().tab;
+        var colors = $(this).data().colors;
+
         $('.content div').removeClass('active');
-        $('#' + tab).addClass('active');      
+        $('#' + tab).addClass('active');   
+        
+        $('.tabs div').removeClass('color');
+        $('#' + colors).addClass('color'); 
     })
+   
 });
